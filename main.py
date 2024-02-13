3c0730c78a527aa43694baea0e2020388eba1efa
DrishyaMody <modydrishya@gmail.com>
2024-02-13 13:53:32 -0800

    delete init posts

--- main.py
+++ main.py
@@ -62,7 +62,6 @@ custom_cli = AppGroup('custom', help='Custom commands')
 def generate_data():
     initUsers()
     initPlayers()
-    #initPosts()
 
 # Register the custom command group with the Flask application
 app.cli.add_command(custom_cli)
